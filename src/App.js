ef72a4d702d89520274597ee77db31269126ae0b
José Antonio Berrón Gutiérrez <61696362+JoseBG-spec@users.noreply.github.com>
2023-01-25 21:09:32 -0600

    Create Basic Layout

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import SimplePaper from './components/TitlebarBelowImageList';
+import DrawerAppBar from './components/DrawerAppBar'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <DrawerAppBar/>
+        
       </header>
     </div>
   );
---
+++ src/components/DrawerAppBar.js
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import TitlebarBelowImageList from './TitlebarBelowImageList';
+
+const drawerWidth = 240;
+const navItems = ['Home', 'About', 'Contact'];
+
+function DrawerAppBar(props) {
+  
+
+
+  return (
+    <Box sx={{ display: 'flex' , width: '80%'}}>
+      <CssBaseline />
+      <AppBar component="nav">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h3"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            Gaming News Page
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box  sx={{ width: '100%', margin: 'auto'}}>
+        <Toolbar />
+        <TitlebarBelowImageList/>
+      </Box>
+    </Box>
+  );
+}
+
+export default DrawerAppBar;
